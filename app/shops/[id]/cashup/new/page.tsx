7297071383383aed485662ce0d5079aae9909012
delipitstop <delipitstop@gmail.com>
2026-05-06 20:29:09 +0100

    Persist cashup week in sessionStorage across navigations

--- app/shops/[id]/cashup/new/page.tsx
+++ app/shops/[id]/cashup/new/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter, useParams, useSearchParams } from 'next/navigation'
 import { createClient } from '@/lib/supabase/client'
 
 type Payout = { description: string; amount: string }
